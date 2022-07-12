1d300901a190d59bc796616dfe2055df9cb9d2a0
Benhur <benhur_zambiasi@hotmail.com>
2022-07-12 18:07:00 -0300

    inicio de criação do dashboard

--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -2,6 +2,8 @@ import { NextPage } from "next";
 import { Header } from "../../components/head";
 import { MasterHeader } from "../../components/masterHeader";
 import { canSSRAuth } from "../../utils/canSSRAuth";
+import { FiRefreshCcw } from "react-icons/fi";
+import styles from "./styles.module.scss";
 
 const Dashboard: NextPage = () => {
   return (
@@ -9,7 +11,22 @@ const Dashboard: NextPage = () => {
       <Header title="Painel - Sujeito Pizzaria" />
       <MasterHeader />
       <div>
-        <main></main>
+        <main className={styles.container}>
+          <div className={styles.containerHeader}>
+            <h1>Ultimos pedidos</h1>
+            <button>
+              <FiRefreshCcw color="#3fffa3" size={25} />
+            </button>
+          </div>
+          <article className={styles.listOrder}>
+            <section className={styles.orderItem}>
+              <button>
+                <div className={styles.tag} />
+                <span>Mesa 30</span>
+              </button>
+            </section>
+          </article>
+        </main>
       </div>
     </div>
   );
